a83c42058eea19191a980f6a46987c0451d40b16
Fransiskus Aldo <rochesterfransiskusaldo@gmail.com>
2024-05-07 18:12:03 -0400

    add daylist to the application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,37 @@
 import React from "react";
 
 import "components/Application.scss";
+import DayLists from "./DayLists";
 
 export default function Application(props) {
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+    {
+      id: 4,
+      name: "Thursday",
+      spots: 3,
+    },
+    {
+      id: 5,
+      name: "Friday",
+      spots: 1,
+    },
+  ];
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +41,15 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayLists
+            days={days}
+            day={"Monday"}
+            setDay={(day) => {
+              console.log(day);
+            }}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
